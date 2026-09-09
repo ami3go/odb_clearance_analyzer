6a1a78a28bc492d2313f216cadae2959a9ef9a77
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-09-09 13:19:12 +0300

    Enable report tab layout patch and bump to 0.4.45

--- odb_clearance_analyzer/__init__.py
+++ odb_clearance_analyzer/__init__.py
@@ -3,6 +3,7 @@
 from .analyzer import ClearanceAnalyzer
 from .run_output import enable_timestamped_report_runs
 from .gui_zone1_default import install_zone1_default_gui_patch
+from .gui_tab_layout import install_report_tab_layout_patch
 from .models import AnalysisCancelled, AnalysisConfig, AnalysisResult, EffectiveAirGapRecord, FeatureAttributeRecord, MeasurementRecord
 
 __all__ = [
@@ -17,5 +18,6 @@ __all__ = [
 
 enable_timestamped_report_runs(ClearanceAnalyzer)
 install_zone1_default_gui_patch()
+install_report_tab_layout_patch()
 
-__version__ = "0.4.44"
+__version__ = "0.4.45"
