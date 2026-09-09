b3170108b48e5df79beea5cfbc0f812373e5e65a
Aleksandr Chasnyk <69671996+ami3go@users.noreply.github.com>
2026-09-09 16:19:15 +0300

    Enable zone matrix duplicate masking

--- odb_clearance_analyzer/__init__.py
+++ odb_clearance_analyzer/__init__.py
@@ -7,6 +7,7 @@ from .gui_tab_layout import install_report_tab_layout_patch
 from .gui_multi_zone import install_multi_zone_support
 from .gui_zones_scroll import install_zones_tab_scroll_support
 from .gui_zone_reset_value import install_zone_reset_value_support
+from .gui_zone_matrix_mask import install_zone_matrix_duplicate_mask_support
 from .models import AnalysisCancelled, AnalysisConfig, AnalysisResult, EffectiveAirGapRecord, FeatureAttributeRecord, MeasurementRecord
 
 __all__ = [
@@ -25,5 +26,6 @@ install_report_tab_layout_patch()
 install_multi_zone_support()
 install_zones_tab_scroll_support()
 install_zone_reset_value_support()
+install_zone_matrix_duplicate_mask_support()
 
-__version__ = "0.4.51"
+__version__ = "0.4.52"
